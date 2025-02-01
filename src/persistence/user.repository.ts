ed9331d65c49cc60874c86b10afbc8e632021f74
geozi <yorgosaz@gmail.com>
2025-02-01 17:36:25 +0200

    build: Added user.repository.

---
+++ src/persistence/user.repository.ts
@@ -0,0 +1,64 @@
+import { Types } from "mongoose";
+import logger from "../../logs/logger.config";
+import { Role } from "../domain/enums/role.enum";
+import { IUser } from "../domain/interfaces/iUser.interface";
+import User from "../domain/models/user.model";
+
+export const getUserByUsername = async (
+  username: string
+): Promise<IUser | null> => {
+  const requestedUser = await User.findOne({ username: username });
+
+  logger.info(`User.findOne({username: username}) called successfully`);
+
+  return requestedUser;
+};
+
+export const getUserByEmail = async (email: string): Promise<IUser | null> => {
+  const requestedUser = await User.findOne({ email: email });
+
+  logger.info(`User.findOne({email: email}) called successfully`);
+
+  return requestedUser;
+};
+
+export const getUsersByRole = async (role: Role): Promise<Array<IUser>> => {
+  const requestedUsers = await User.find({ role: role });
+
+  logger.info(`User.find({role:role}) called successfully`);
+
+  return requestedUsers;
+};
+
+export const addUser = async (newUser: IUser): Promise<IUser> => {
+  const savedUser = await newUser.save();
+
+  logger.info(`User.prototype.save() called successfully`);
+
+  return savedUser;
+};
+
+export const updateUser = async (
+  id: Types.ObjectId,
+  updateDataObject: object
+): Promise<IUser | null> => {
+  const updatedUser = await User.findByIdAndUpdate(id, updateDataObject, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  });
+
+  logger.info(
+    `User.findByIdAndUpdate(id, updateDataObject) called successfully`
+  );
+
+  return updatedUser;
+};
+
+export const deleteUser = async (id: Types.ObjectId): Promise<IUser | null> => {
+  const deletedUser = await User.findByIdAndDelete(id);
+
+  logger.info(`User.findByIdAndDelete(id) called successfully`);
+
+  return deletedUser;
+};
