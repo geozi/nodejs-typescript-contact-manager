0e942adb1d37a849c237259fed8c1470f518606b
geozi <yorgosaz@gmail.com>
2025-02-02 17:40:35 +0200

    docs: Added typedocs in src/domain/errors.

--- src/domain/errors/uniqueConstraint.error.ts
+++ src/domain/errors/uniqueConstraint.error.ts
@@ -1,6 +1,19 @@
+/**
+ * Unique constraint error.
+ * @module src/domain/errors/uniqueConstraint.error
+ */
+
+/**
+ * Custom error class for violation of uniqueness.
+ * @extends {Error}
+ */
 export class UniqueConstraintError extends Error {
   public static httpCode = 409;
 
+  /**
+   * Creates an instance of UniqueConstraintError.
+   * @param {string} message The error message.
+   */
   constructor(message: string) {
     super(message);
     this.name = "UniqueConstraintError";
