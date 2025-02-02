b99c5d9d9db2878bf10a21b55b2db34da6411ae1
geozi <yorgosaz@gmail.com>
2025-02-02 17:37:46 +0200

    docs: Added typedocs in src/domain/enums.

--- src/domain/enums/role.enum.ts
+++ src/domain/enums/role.enum.ts
@@ -1,4 +1,26 @@
+/**
+ * Role enum.
+ * @module src/domain/enums/role.enum
+ */
+
+/**
+ * Enum for User roles.
+ *
+ * @readonly
+ * @enum
+ */
 export enum Role {
+  /**
+   * Admin role.
+   * @readonly
+   * @type {string}
+   */
   Admin = "Admin",
+
+  /**
+   * User role.
+   * @readonly
+   * @type {string}
+   */
   User = "User",
 }
