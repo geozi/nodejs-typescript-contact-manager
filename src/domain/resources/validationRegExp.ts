bd2dd891c44af0ab78262443381aa7199281cbd3
geozi <yorgosaz@gmail.com>
2025-02-02 19:57:23 +0200

    docs: Added typedocs to src/domain/resources.

--- src/domain/resources/validationRegExp.ts
+++ src/domain/resources/validationRegExp.ts
@@ -1,15 +1,44 @@
+/**
+ * Regular expressions used in validation processes.
+ * @module src/domain/resources/validationRegExp
+ */
+
+/**
+ * Regular expression used in checking password validity.
+ * @type {RegExp}
+ */
 export const PASSWORD_REGEX = new RegExp(
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/
 );
 
+/**
+ * Regular expression used in checking email validity.
+ * @type {RegExp}
+ */
 export const EMAIL_REGEX = new RegExp(
   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 );
 
+/**
+ * Regular expression used in checking firstName and lastName validity.
+ * @type {RegExp}
+ */
 export const NAME_REGEX = new RegExp(/^[A-Za-z]+$/);
 
+/**
+ * Regular expression used in checking zipCode validity.
+ * @type {RegExp}
+ */
 export const ZIP_CODE_REGEX = new RegExp(/^\d+$/);
 
+/**
+ * Regular expression used in checking phoneNumber validity.
+ * @type {RegExp}
+ */
 export const PHONE_REGEX = new RegExp(/^\d+(-\d+)*$/);
 
+/**
+ * Regular expression used in checking id validity.
+ * @type {RegExp}
+ */
 export const ID_REGEX = new RegExp(/^[a-fA-F0-9]+$/);
