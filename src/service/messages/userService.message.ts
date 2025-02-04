06bff503169ce175d7aab475fbef6feead5e625e
geozi <yorgosaz@gmail.com>
2025-02-04 11:29:36 +0200

    fix: removed messages from userService.message.

--- src/service/messages/userService.message.ts
+++ src/service/messages/userService.message.ts
@@ -1,6 +1,4 @@
 export const userServiceMessages = {
   USER_NOT_FOUND: "User was not found",
   USERS_NOT_FOUND: "Users were not found",
-  USER_PROFILE_CREATED: "Successful creation of user profile",
-  USER_PROFILE_UPDATED: "Successful update of user profile",
 };
