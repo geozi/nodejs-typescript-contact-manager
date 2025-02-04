1540bf8bb13a6831c66b30a02b0810388eb6712a
geozi <yorgosaz@gmail.com>
2025-02-04 10:18:35 +0200

    build: Added common response messages and response messages for user.service.

---
+++ src/service/messages/commonService.message.ts
@@ -0,0 +1,3 @@
+export const commonServiceMessages = {
+  SERVER_ERROR: "Internal server error",
+};
---
+++ src/service/messages/userService.message.ts
@@ -0,0 +1,6 @@
+export const userServiceMessages = {
+  USER_NOT_FOUND: "User was not found",
+  USERS_NOT_FOUND: "Users were not found",
+  USER_PROFILE_CREATED: "Successful creation of user profile",
+  USER_PROFILE_UPDATED: "Successful update of user profile",
+};
