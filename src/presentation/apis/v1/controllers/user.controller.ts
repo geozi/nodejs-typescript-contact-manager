594c7d28fd1b00dc9b50ad762b54e0f626c08bb2
geozi <yorgosaz@gmail.com>
2025-02-06 17:21:35 +0200

    build: Added user.controller; USER_RETRIEVED and USER_S_RETRIEVED to controller response messages.

---
+++ src/presentation/apis/v1/controllers/user.controller.ts
@@ -0,0 +1,231 @@
+import { validationResult } from "express-validator";
+import {
+  userProfileDeleteRules,
+  userProfileUpdateRules,
+  userRegistrationRules,
+  userRetrievalByEmailRules,
+  userRetrievalByRoleRules,
+  userRetrievalByUsernameRules,
+} from "../middleware/user.rules";
+import { Request, Response } from "express";
+import { controllerResponseMessages } from "../../../messages/controllerResponse.message";
+import User from "../../../../domain/models/user.model";
+import {
+  createUserProfile,
+  deleteUserProfile,
+  retrieveUserByEmail,
+  retrieveUserByUsername,
+  retrieveUsersByRole,
+  updateUserProfile,
+} from "../../../../service/user.service";
+import { httpCodes } from "../../../codes/responseStatusCodes";
+import { ServerError } from "../../../../errors/serverError.class";
+import { UniqueConstraintError } from "../../../../errors/uniqueConstraint.error";
+import { NotFoundError } from "../../../../errors/notFoundError.class";
+import { IUserUpdate } from "../../../interfaces/iUserUpdate.interface";
+import { Types } from "mongoose";
+
+export const registerUser = [
+  ...userRegistrationRules(),
+  async (req: Request, res: Response): Promise<void> => {
+    const expressErrors = validationResult(req);
+    if (!expressErrors.isEmpty()) {
+      const errorMessage = expressErrors.array().map((err) => ({
+        message: err.msg,
+      }));
+
+      res.status(httpCodes.BAD_REQUEST).json({
+        message: controllerResponseMessages.BAD_REQUEST,
+        errors: errorMessage,
+      });
+      return;
+    }
+
+    try {
+      const { username, email, password, role } = req.body;
+      const newUser = new User({
+        username: username,
+        email: email,
+        password: password,
+        role: role,
+      });
+      await createUserProfile(newUser);
+      res
+        .status(httpCodes.CREATED)
+        .json({ message: controllerResponseMessages.USER_REGISTERED });
+    } catch (error: ServerError | UniqueConstraintError | unknown) {
+      if (
+        error instanceof ServerError ||
+        error instanceof UniqueConstraintError
+      ) {
+        res.status(error.httpCode).json({ message: error.message });
+        return;
+      }
+    }
+  },
+];
+
+export const updateUserInfo = [
+  ...userProfileUpdateRules(),
+  async (req: Request, res: Response): Promise<void> => {
+    const expressErrors = validationResult(req);
+    if (!expressErrors.isEmpty()) {
+      const errorMessage = expressErrors.array().map((err) => ({
+        message: err.msg,
+      }));
+
+      res.status(httpCodes.BAD_REQUEST).json({
+        message: controllerResponseMessages.BAD_REQUEST,
+        errors: errorMessage,
+      });
+      return;
+    }
+
+    try {
+      const { id, username, email, password } = req.body;
+      const idAsObjectId = new Types.ObjectId(id);
+      const userUpdateInfo: IUserUpdate = {
+        username: username,
+        email: email,
+        password: password,
+      };
+
+      const updatedUser = await updateUserProfile(idAsObjectId, userUpdateInfo);
+      res.status(httpCodes.OK).json({
+        message: controllerResponseMessages.USER_UPDATED,
+        data: updatedUser,
+      });
+    } catch (error: NotFoundError | ServerError | unknown) {
+      if (error instanceof NotFoundError || error instanceof ServerError) {
+        res.status(error.httpCode).json({ message: error.message });
+        return;
+      }
+    }
+  },
+];
+
+export const deleteUserInfo = [
+  ...userProfileDeleteRules(),
+  async (req: Request, res: Response): Promise<void> => {
+    const expressErrors = validationResult(req);
+    if (!expressErrors.isEmpty()) {
+      const errorMessage = expressErrors.array().map((err) => ({
+        message: err.msg,
+      }));
+
+      res.status(httpCodes.BAD_REQUEST).json({
+        message: controllerResponseMessages.BAD_REQUEST,
+        errors: errorMessage,
+      });
+      return;
+    }
+
+    try {
+      const { id } = req.body;
+      const idAsObjectId = new Types.ObjectId(id);
+      await deleteUserProfile(idAsObjectId);
+      res.status(httpCodes.NO_CONTENT).json({});
+    } catch (error: NotFoundError | ServerError | unknown) {
+      if (error instanceof NotFoundError || error instanceof ServerError) {
+        res.status(error.httpCode).json({ message: error.message });
+        return;
+      }
+    }
+  },
+];
+
+export const fetchUserByUsername = [
+  ...userRetrievalByUsernameRules(),
+  async (req: Request, res: Response): Promise<void> => {
+    const expressErrors = validationResult(req);
+    if (!expressErrors.isEmpty()) {
+      const errorMessage = expressErrors.array().map((err) => ({
+        message: err.msg,
+      }));
+
+      res.status(httpCodes.BAD_REQUEST).json({
+        message: controllerResponseMessages.BAD_REQUEST,
+        errors: errorMessage,
+      });
+      return;
+    }
+
+    try {
+      const { username } = req.body;
+      const user = await retrieveUserByUsername(username);
+      res.status(httpCodes.OK).json({
+        message: controllerResponseMessages.USER_RETRIEVED,
+        data: user,
+      });
+    } catch (error: NotFoundError | ServerError | unknown) {
+      if (error instanceof NotFoundError || error instanceof ServerError) {
+        res.status(error.httpCode).json({ message: error.message });
+        return;
+      }
+    }
+  },
+];
+
+export const fetchUserByEmail = [
+  ...userRetrievalByEmailRules(),
+  async (req: Request, res: Response): Promise<void> => {
+    const expressErrors = validationResult(req);
+    if (!expressErrors.isEmpty()) {
+      const errorMessage = expressErrors.array().map((err) => ({
+        message: err.msg,
+      }));
+
+      res.status(httpCodes.BAD_REQUEST).json({
+        message: controllerResponseMessages.BAD_REQUEST,
+        errors: errorMessage,
+      });
+      return;
+    }
+
+    try {
+      const { email } = req.body;
+      const user = await retrieveUserByEmail(email);
+      res.status(httpCodes.OK).json({
+        message: controllerResponseMessages.USER_RETRIEVED,
+        data: user,
+      });
+    } catch (error: NotFoundError | ServerError | unknown) {
+      if (error instanceof NotFoundError || error instanceof ServerError) {
+        res.status(error.httpCode).json({ message: error.message });
+        return;
+      }
+    }
+  },
+];
+
+export const fetchUsersByRole = [
+  ...userRetrievalByRoleRules(),
+  async (req: Request, res: Response): Promise<void> => {
+    const expressErrors = validationResult(req);
+    if (!expressErrors.isEmpty()) {
+      const errorMessage = expressErrors.array().map((err) => ({
+        message: err.msg,
+      }));
+
+      res.status(httpCodes.BAD_REQUEST).json({
+        message: controllerResponseMessages.BAD_REQUEST,
+        errors: errorMessage,
+      });
+      return;
+    }
+
+    try {
+      const { role } = req.body;
+      const users = await retrieveUsersByRole(role);
+      res.status(httpCodes.OK).json({
+        message: controllerResponseMessages.USER_S_RETRIEVED,
+        data: users,
+      });
+    } catch (error: NotFoundError | ServerError | unknown) {
+      if (error instanceof NotFoundError || error instanceof ServerError) {
+        res.status(error.httpCode).json({ message: error.message });
+        return;
+      }
+    }
+  },
+];
--- src/presentation/messages/controllerResponse.message.ts
+++ src/presentation/messages/controllerResponse.message.ts
@@ -1,5 +1,7 @@
 export const controllerResponseMessages = {
   USER_REGISTERED: "Successful user registration",
   USER_UPDATED: "Successful user update",
+  USER_RETRIEVED: "Successful user retrieval",
+  USER_S_RETRIEVED: "Successful retrieval of users",
   BAD_REQUEST: "Bad request",
 };
