f433c8127859c039a54a91ac5c343f00b30baca5
geozi <yorgosaz@gmail.com>
2025-02-08 13:04:48 +0200

    build: Added contactController messages.

---
+++ src/presentation/messages/contactControllerReponse.message.ts
@@ -0,0 +1,5 @@
+export const contactControllerResponseMessages = {
+  CONTACT_REGISTERED: "Successful contact registration",
+  CONTACT_UPDATED: "Successful contact update",
+  CONTACT_RETRIEVED: "Successful contact retrieval",
+};
