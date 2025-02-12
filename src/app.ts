9562fc41ed766a52aef950a99b8903fcd07a7204
geozi <yorgosaz@gmail.com>
2025-02-12 16:38:54 +0200

    build: Imported routers to app.ts.

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
+import { authRouter } from "./presentation/apis/v1/routes/auth.route";
+import { userRouter } from "./presentation/apis/v1/routes/user.route";
+import { groupRouter } from "./presentation/apis/v1/routes/group.route";
+import { contactRouter } from "./presentation/apis/v1/routes/contact.route";
 dotenv.config();
 const app = express();
 const port = 3000;
@@ -17,6 +21,12 @@ async function connectToDB() {
     console.log("Failed to connect to database", error);
   }
 }
+
+app.use(express.json());
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/groups", groupRouter);
+app.use("/api/v1/contacts", contactRouter);
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
